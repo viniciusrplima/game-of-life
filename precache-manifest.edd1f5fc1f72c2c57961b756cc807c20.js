f0a0436d37705b05d4d62f0fe19fcf08bc3e3efa
Vinicius Rodrigues <vinicius.rodrigues.lima@ccc.ufcg.edu.br>
2020-06-21 11:34:12 -0300

    Updates

---
+++ precache-manifest.edd1f5fc1f72c2c57961b756cc807c20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de795f692f42b627b39d2b22fc69c727",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "1377c5b726867a83fbfa",
+    "url": "/game-of-life/static/css/main.db10c711.chunk.css"
+  },
+  {
+    "revision": "6359191eab472344730e",
+    "url": "/game-of-life/static/js/2.a908165e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/game-of-life/static/js/2.a908165e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1377c5b726867a83fbfa",
+    "url": "/game-of-life/static/js/main.aa0240f7.chunk.js"
+  },
+  {
+    "revision": "39f8af036aee07664d58",
+    "url": "/game-of-life/static/js/runtime-main.1c12863f.js"
+  }
+]);
